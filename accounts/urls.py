6e1be8097084fca3738f93c0931d82d8ccf81c7f
SomeIntersum <deminwit@gmail.com>
2024-09-30 11:52:35 +1200

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from accounts.views import login_page, register_page, user_logout
-
-urlpatterns = [
-    path('login/', login_page, name='login_page'),
-    path('register/', register_page, name='register'),
-    path('logout/', user_logout, name='logout'),
-]
